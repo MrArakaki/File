ada00c54b9044ce3d9ab8a97d328834212bf25d4
Marcelo Ribeiro <arakaki2k2@gmail.com>
2020-02-12 17:03:30 -0300

    file_scanner

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,31 @@
 package application;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		File file = new File();
+		File file = new File("c:\\temp\\ws_scanner\\scanner\\teste_scanner_file.txt");
+		Scanner sc=null;
+		try {
+			sc = new Scanner(file);
+			while(sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+			
+		}
+		catch (IOException e ) {
+			System.out.println("Error: " + e.getMessage());
+		}
+		finally {
+			if(sc !=null) {
+				sc.close();
+			}
+		}
 
 	}
 
