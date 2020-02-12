eeca8c67ef7a4b5752480b6f48e728b64913ecb0
Marcelo Ribeiro <arakaki2k2@gmail.com>
2020-02-12 16:54:30 -0300

    file_scanner

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import java.io.File;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		File file = new File();
+
+	}
+
+}
